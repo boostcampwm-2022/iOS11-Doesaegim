6c843253e49d9f8d794fa0af89a1c3a777a4f03d
Jaehoon-So <jhspacelover@naver.com>
2022-11-10 14:11:38 +0900

    chore: 프로젝트 초기설정

---
+++ Doesaegim/Doesaegim/App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Doesaegim
+//
+//  Created by Jaehoon So on 2022/11/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
