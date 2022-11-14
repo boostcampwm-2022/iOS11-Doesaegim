16734570f03168df77f057f6366ccee0cf8c19ec
Jaehoon-So <jhspacelover@naver.com>
2022-11-14 21:01:42 +0900

    feat: 중앙 placehold 라벨 추가

--- Doesaegim/Doesaegim/Presentation/TravelScene/TravelPlanViewController.swift
+++ Doesaegim/Doesaegim/Presentation/TravelScene/TravelPlanViewController.swift
@@ -6,24 +6,39 @@
 //
 
 import UIKit
+import SnapKit
 
 final class TravelPlanViewController: UIViewController {
 
+    
+    let placeholdLabel: UILabel = {
+        let label = UILabel()
+        label.text = "새로운 여행을 떠나볼까요?"
+        label.textColor = .grey2
+        
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
+        view.backgroundColor = .white
         // Do any additional setup after loading the view.
+        configureSubviews()
+        configureConstraints()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Configure
+    
+    func configureSubviews() {
+        view.addSubview(placeholdLabel)
     }
-    */
+    
+    func configureConstraints() {
+        placeholdLabel.snp.makeConstraints {
+            $0.centerX.equalTo(view.snp.centerX)
+            $0.centerY.equalTo(view.snp.centerY)
+        }
+    }
+    
 
 }
