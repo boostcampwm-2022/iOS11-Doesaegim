515f4a530ead22794f6106dee8b1173310b1a6b2
sun <skkimeo@gmail.com>
2022-11-14 23:38:41 +0900

    feat: PlanCollectionViewCell 구현

---
+++ Doesaegim/Doesaegim/Presentation/TravelScene/PlanList/View/PlanCollectionViewCell.swift
@@ -0,0 +1,160 @@
+//
+//  PlanCollectionViewCell.swift
+//  Doesaegim
+//
+//  Created by sun on 2022/11/14.
+//
+
+import UIKit
+
+import SnapKit
+
+final class PlanCollectionViewCell: UICollectionViewCell {
+
+    // MARK: - UI Properties
+
+    private let checkBox = CheckBox()
+
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = label.font.withSize(FontSize.body)
+        label.numberOfLines = .zero
+        label.lineBreakMode = .byWordWrapping
+
+        return label
+    }()
+
+    private let timeLabel: UILabel = {
+        let label = UILabel()
+        label.font = label.font.withSize(FontSize.caption)
+        label.textColor = .grey2
+        label.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+
+        return label
+    }()
+
+    private let locationLabel: UILabel = {
+        let label = UILabel()
+        label.font = label.font.withSize(FontSize.caption)
+        label.numberOfLines = .zero
+        label.lineBreakMode = .byWordWrapping
+        label.textColor = .grey2
+
+        return label
+    }()
+
+    private let contentLabel: UILabel = {
+        let label = UILabel()
+        label.font = label.font.withSize(FontSize.caption)
+        label.numberOfLines = .zero
+        label.lineBreakMode = .byWordWrapping
+        label.textColor = .grey2
+
+        return label
+    }()
+
+    /// 체크박스와 모든 레이블을 담고 있는 최상위 스택 뷰
+    private let contentStack: UIStackView = {
+        let view = UIStackView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.spacing = Metric.contentStackSpacing
+
+        return view
+    }()
+
+    /// 레이블들만 담고 있는 스택뷰
+    private let labelStack: UIStackView = {
+        let view = UIStackView()
+        view.spacing = Metric.spacing
+        view.axis = .vertical
+
+        return view
+    }()
+
+    private let nameAndTimeStack: UIStackView = {
+        let view = UIStackView()
+        view.spacing = Metric.spacing
+
+        return view
+    }()
+
+
+    // MARK: - Properties
+
+    var viewModel: PlanViewModel? {
+        didSet {
+            render()
+        }
+    }
+
+
+    // MARK: - Init(s)
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        configureViews()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+
+        configureViews()
+    }
+
+
+    // MARK: - Life Cycle
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+
+        render()
+    }
+
+
+    // MARK: - Rendering Functions
+
+    /// 뷰모델로부터 데이터를 받아와 그림, 뷰모델이 없는 경우 전부 nil로 초기화
+    func render() {
+        checkBox.isChecked = viewModel?.isComplete == true
+        nameLabel.text = viewModel?.name
+        timeLabel.text = viewModel?.timeString
+        locationLabel.text = viewModel?.location
+        contentLabel.text = viewModel?.content
+    }
+
+
+    // MARK: - Configuration Functions
+
+    private func configureViews() {
+        configureSubviews()
+        configureConstraints()
+    }
+
+    private func configureSubviews() {
+        contentView.addSubview(contentStack)
+        contentStack.addArrangedSubviews(checkBox, labelStack)
+        labelStack.addArrangedSubviews(nameAndTimeStack, locationLabel, contentLabel)
+        nameAndTimeStack.addArrangedSubviews(nameLabel, timeLabel)
+    }
+
+    private func configureConstraints() {
+        contentStack.snp.makeConstraints { $0.edges.equalToSuperview() }
+    }
+}
+
+
+// MARK: - Constants
+fileprivate extension PlanCollectionViewCell {
+
+    enum Metric {
+
+        static let spacing: CGFloat = 4
+
+        static let contentStackSpacing: CGFloat = 8
+
+        static let horizontalInset: CGFloat = 20
+
+        static let checkBoxWidth: CGFloat = 30
+    }
+}
