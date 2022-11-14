76add4f89a568d43f7e75f3473d810500fc1f3db
Jaehoon-So <jhspacelover@naver.com>
2022-11-14 22:55:16 +0900

    feat: View출력용 TravelInfoViewModel 생성

--- Doesaegim/Doesaegim/Presentation/TravelScene/TravelInfoViewModel.swift
+++ Doesaegim/Doesaegim/Presentation/TravelScene/TravelInfoViewModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct TravelInfoViewModel: Hashable {
+    var uuid: UUID
+    var title: String
+    var startDate: Date
+    var endDate: Date
+    
+    init(uuid: UUID, title: String, startDate: Date, endDate: Date) {
+        self.uuid = uuid
+        self.title = title
+        self.startDate = startDate
+        self.endDate = endDate
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(uuid)
+    }
+}
