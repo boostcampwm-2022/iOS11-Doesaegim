b88806ec7eff7f181e3e9b005809dc4e1b8ab611
Jaehoon-So <jhspacelover@naver.com>
2022-11-15 21:28:01 +0900

    feat: 여행 날짜 문자열 Date Extension 정의

---
+++ Doesaegim/Doesaegim/Presentation/ExpenseScene/ExpenseTravelList/View/ExpenseTravelViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ExpenseTravelViewCell.swift
+//  Doesaegim
+//
+//  Created by Jaehoon So on 2022/11/15.
+//
+
+import UIKit
+
+class ExpenseTravelViewCell: UICollectionViewListCell {
+    
+}
