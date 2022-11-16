bea434a88642b398b05144baefc8aeb27a38ede5
sun <skkimeo@gmail.com>
2022-11-17 00:06:20 +0900

    feat: 일정 추가 버튼 구현

--- Doesaegim/Doesaegim/Presentation/TravelScene/PlanList/View/PlanListViewController.swift
+++ Doesaegim/Doesaegim/Presentation/TravelScene/PlanList/View/PlanListViewController.swift
@@ -60,6 +60,10 @@ final class PlanListViewController: UIViewController {
 
     private func configureNavigationBar() {
         navigationItem.title = viewModel.navigationTitle
+        // TODO: 일정 추가 화면으로 교체
+        let nextViewController = UIViewController()
+        nextViewController.view.backgroundColor = .systemYellow
+        setRightBarAddButton(showing: nextViewController)
     }
 
 
---
+++ Doesaegim/Doesaegim/Utility/Extensions/UIViewController+SetRightBarPlusButton.swift
@@ -0,0 +1,23 @@
+//
+//  UIViewController+SetRightBarPlusButton.swift
+//  Doesaegim
+//
+//  Created by sun on 2022/11/16.
+//
+
+import UIKit
+
+extension UIViewController {
+
+    /// 누르면 항목을 추가할 수 있는 뷰를 띄우는 우측 내비게이션 바 버튼을 설정
+    ///
+    /// 제목은 없고 + 아이콘만 가짐
+    /// - Parameter viewController: 추가 버튼을 누르면 띄울 뷰 컨트롤러
+    func setRightBarAddButton(showing viewController: UIViewController) {
+        let addButton = UIBarButtonItem(systemItem: .add, primaryAction: UIAction(handler: { _ in
+            self.show(viewController, sender: self)
+        }))
+        addButton.tintColor = .black
+        navigationItem.setRightBarButton(addButton, animated: true)
+    }
+}
