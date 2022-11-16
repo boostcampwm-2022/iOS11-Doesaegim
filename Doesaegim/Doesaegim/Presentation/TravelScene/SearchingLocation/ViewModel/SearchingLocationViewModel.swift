1e789609510b3d4f6b0d9c73c3e87fd8227b5e27
bokyungseo <maybutter756@gmail.com>
2022-11-16 19:59:16 +0900

    refactor: MKLocalSearch.Request 지역 설정 로직 수정

--- Doesaegim/Doesaegim/Presentation/TravelScene/SearchingLocation/ViewModel/SearchingLocationViewModel.swift
+++ Doesaegim/Doesaegim/Presentation/TravelScene/SearchingLocation/ViewModel/SearchingLocationViewModel.swift
@@ -25,17 +25,19 @@ final class SearchingLocationViewModel {
     /// 키워드를 통해 장소를 검색하고, 검색 결과값을 `searchResultCellViewModels`에 저장한다.
     /// - Parameter keyword: 입력된 키워드
     func fetchSearchResults(with keyword: String) {
-        // TODO: MKMapView 인스턴스를 생성만 하면 현재 위치 정보도 여기에 포함되는 걸까..? 아니라면 사용자의 현재 위치에 맞는 검색 결과를 보여주도록 수정 필요..
-        let mapView = MKMapView()
         let request = MKLocalSearch.Request()
         request.naturalLanguageQuery = keyword
-        request.region = mapView.region
+        request.region = MKCoordinateRegion(MKMapRect.world)
         
         let search = MKLocalSearch(request: request)
-        search.start { response, _ in
-            guard let response = response else {
+        search.start { response, error in
+            guard let response = response, error == nil
+            else {
+                // TODO: 에러 처리 필요
+                self.searchResultCellViewModels = []
                 return
             }
+            
             let cellViewModels = response.mapItems.map({
                 let placemark = $0.placemark
                 return SearchResultCellViewModel(
@@ -45,9 +47,6 @@ final class SearchingLocationViewModel {
                     longitude: placemark.coordinate.longitude
                 )
             })
-            mapView.removeAnnotations(mapView.annotations)
-            // [Memory] Resetting zone allocator with 32387 allocations still alive 경고가 떠서 추가
-            // removeAnnotations과 어떤 관련이 있는지는 아직 정확히 파악하지 못함..
             
             self.searchResultCellViewModels = cellViewModels
         }
