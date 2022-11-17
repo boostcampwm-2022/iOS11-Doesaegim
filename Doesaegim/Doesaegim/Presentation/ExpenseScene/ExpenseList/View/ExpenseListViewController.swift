aceade7aebf03a2501f5c17aabcececec366e04a
Jaehoon-So <jhspacelover@naver.com>
2022-11-17 15:32:18 +0900

    feat: ExpensePlaceholdView 정의

--- Doesaegim/Doesaegim/Presentation/ExpenseScene/ExpenseList/View/ExpenseListViewController.swift
+++ Doesaegim/Doesaegim/Presentation/ExpenseScene/ExpenseList/View/ExpenseListViewController.swift
@@ -9,21 +9,54 @@ import UIKit
 
 class ExpenseListViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    
+    let expenseCollectionView: UICollectionView = {
+        var configuration = UICollectionLayoutListConfiguration(appearance: .plain)
+        configuration.showsSeparators = true
+//        configuration.trailingSwipeActionsConfigurationProvider
+        let layout = UICollectionViewCompositionalLayout.list(using: configuration)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        // TODO: - cornerRadius 적용되지 않는 것 고민
+        collectionView.backgroundColor = .blue
+        collectionView.layer.cornerRadius = 7
+        return collectionView
+    }()
+    
+    var travelID: UUID? // 여행목록에서 선택한 여행의 ID. 지출목록을 가져오는데에 사용한다.
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureNavigationBar()
+        configureSubviews()
+        configureConstraints()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Configuration
+    
+    private func configureNavigationBar() {
+        // TODO: - 여행 이름도 네비게이션 타이틀에 포함하기 "(여행이름) - 지출내역"
+        navigationItem.title = "지출 내역"
     }
-    */
-
+    
+    private func configureSubviews() {
+        view.addSubview(expenseCollectionView)
+//        view.addSubview(placeholdLabel)
+    }
+    
+    private func configureConstraints() {
+        expenseCollectionView.snp.makeConstraints {
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            $0.verticalEdges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    // MARK: - Collection View
+    
 }
--- Doesaegim/Doesaegim/Presentation/ExpenseScene/ExpenseList/View/ExpensePlaceholdView.swift
+++ Doesaegim/Doesaegim/Presentation/ExpenseScene/ExpenseList/View/ExpensePlaceholdView.swift
@@ -5,8 +5,54 @@
 //  Created by Jaehoon So on 2022/11/17.
 //
 
-import Foundation
+import UIKit
 
 final class ExpensePlaceholdView: UIView {
     
+    // MARK: - Properties
+    
+    let mainLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .primaryOrange
+        label.text = "지출 내역이 없어요!"
+        
+        return label
+    }()
+    
+    // MARK: - Initializer
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: - Configuration
+    
+    private func configure() {
+        configureSubviews()
+        configureStyle()
+        configureContstraints()
+    }
+    
+    private func configureSubviews() {
+        addSubview(mainLabel)
+    }
+    
+    private func configureStyle() {
+        layer.cornerRadius = 7
+        layer.borderColor = UIColor.primaryOrange?.cgColor
+        layer.borderWidth = 1
+    }
+    
+    private func configureContstraints() {
+        mainLabel.snp.makeConstraints {
+            $0.centerX.equalTo(self.snp.centerX)
+            $0.centerY.equalTo(self.snp.centerX)
+        }
+    }
 }
