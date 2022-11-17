f7fec920d13550472b8316780211fe059f46ba94
Jaehoon-So <jhspacelover@naver.com>
2022-11-17 14:01:00 +0900

    feat: 지출여행목록 커스텀 셀 파일 생성

---
+++ Doesaegim/Doesaegim/Presentation/ExpenseScene/ExpenseTravelList/View/ExpenseTravelListCell.swift
@@ -0,0 +1,12 @@
+//
+//  ExpenseTravelListCell.swift
+//  Doesaegim
+//
+//  Created by Jaehoon So on 2022/11/17.
+//
+
+import UIKit
+
+final class ExpenseTravelListCell: UICollectionViewListCell {
+    
+}
