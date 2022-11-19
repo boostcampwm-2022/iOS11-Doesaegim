7037e047627be38cbfda037e36b9e65b15ef0115
Jaehoon-So <jhspacelover@naver.com>
2022-11-20 00:48:47 +0900

    feat: 지출 목록 뷰모델 프로토콜 작성

--- Doesaegim/Doesaegim/Presentation/ExpenseScene/ExpenseList/ExpenseListViewModelProtocol.swift
+++ Doesaegim/Doesaegim/Presentation/ExpenseScene/ExpenseList/ExpenseListViewModelProtocol.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+protocol ExpenseListViewModelProtocol: AnyObject {
+    
+    var delegate: ExpenseListViewModelDelegate? { get set }
+    var expenseInfos: [ExpenseInfoViewModel] { get set }
+    
+    func fetchExpenseData(with travelID: UUID)
+    
+}
+
+protocol ExpenseListViewModelDelegate: AnyObject {
+    
+    /// 지출 항목에 변화가 생겼을 때 호출되는 delegate 함수입니다.
+    /// `expenseInfos` 데이터를 바탕으로 `UICollectionView`의 스냅샷을 반영하는데에 사용합니다.
+    func expenseListDidChanged()
+    
+}
