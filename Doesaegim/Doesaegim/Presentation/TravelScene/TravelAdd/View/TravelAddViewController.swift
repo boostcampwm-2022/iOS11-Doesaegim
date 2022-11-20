686d1c0a4370e5008c72d50ed81063203b7e27cf
mandos1995 <mandos19950620@gmail.com>
2022-11-20 20:59:49 +0900

    refactor: @available을 사용하여 비가용성 정의

--- Doesaegim/Doesaegim/Presentation/TravelScene/TravelAdd/View/TravelAddViewController.swift
+++ Doesaegim/Doesaegim/Presentation/TravelScene/TravelAdd/View/TravelAddViewController.swift
@@ -167,6 +167,7 @@ final class TravelAddViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
--- Doesaegim/Doesaegim/Utility/Custom/CustomCalendar.swift
+++ Doesaegim/Doesaegim/Utility/Custom/CustomCalendar.swift
@@ -53,6 +53,7 @@ final class CustomCalendar: UICollectionView {
         configureSnapshot()
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
