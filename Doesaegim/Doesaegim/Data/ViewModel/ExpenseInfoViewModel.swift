e170f03fb4634a0ed22d0311c7956f0d5816488d
Jaehoon-So <jhspacelover@naver.com>
2022-11-21 19:02:57 +0900

    feat: ExpenseType 열거형 추가

--- Doesaegim/Doesaegim/Data/ViewModel/ExpenseInfoViewModel.swift
+++ Doesaegim/Doesaegim/Data/ViewModel/ExpenseInfoViewModel.swift
@@ -34,3 +34,19 @@ struct ExpenseInfoViewModel: Hashable {
         hasher.combine(uuid)
     }
 }
+
+// TODO: - 정수로 관리하고싶음
+//enum ExpenseType: Int {
+//    case food = 0
+//    case transportation = 1
+//    case room = 2
+//    case other = 3
+//}
+
+// MARK: - ExpenseType: 지금은 문자열로 구분
+enum ExpenseType: String {
+    case food = "식비"
+    case transportation = "교통비"
+    case room = "숙박비"
+    case other = "기타"
+}
