243f89975e10104b3b926d6b53ea8d60f2d53398
sun <skkimeo@gmail.com>
2022-11-21 13:07:34 +0900

    refactor: 여행목록에 PersistentManager 리턴 타입 변경 반영

--- Doesaegim/Doesaegim/Repository/TravelScene/PlanList/PlanRepository.swift
+++ Doesaegim/Doesaegim/Repository/TravelScene/PlanList/PlanRepository.swift
@@ -10,9 +10,9 @@ import Foundation
 /// Plan 엔티티에 대한 CRUD 작업을 수행하는 저장소
 protocol PlanRepository {
 
-    func fetchPlans(ofTravelID id: UUID, batchSize: Int) throws -> [Plan]
+    func fetchPlans(ofTravelID id: UUID, batchSize: Int) -> Result<[Plan], Error>
 
-    func save() throws
+    func save() -> Result<Bool, Error>
 
-    func deletePlan(_ plan: Plan) throws
+    func deletePlan(_ plan: Plan) -> Result<Bool, Error>
 }
