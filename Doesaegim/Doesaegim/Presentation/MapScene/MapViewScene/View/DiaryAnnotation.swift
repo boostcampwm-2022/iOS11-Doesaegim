ab7ef29c89ec9121f6958c6812e813ee46563e4b
Jaehoon-So <jhspacelover@naver.com>
2022-11-22 23:23:07 +0900

    feat: 어노테이션 프로퍼티 추가

--- Doesaegim/Doesaegim/Presentation/MapScene/MapViewScene/View/DiaryAnnotation.swift
+++ Doesaegim/Doesaegim/Presentation/MapScene/MapViewScene/View/DiaryAnnotation.swift
@@ -13,9 +13,10 @@ final class DiaryAnnotation: NSObject, MKAnnotation {
     
     @objc dynamic var coordinate: CLLocationCoordinate2D
     
+    var id: UUID?
     var title: String?
     var subtitle: String?
-    var imageName: String?
+    var imagePath: String?
     
     init(coordinate: CLLocationCoordinate2D) {
         self.coordinate = coordinate
@@ -25,7 +26,11 @@ final class DiaryAnnotation: NSObject, MKAnnotation {
 
 extension DiaryAnnotation {
     func configure(with diaryInfo: DiaryMapInfoViewModel) {
+        id = diaryInfo.id
         title = diaryInfo.title
         subtitle = diaryInfo.content // 내용을 나타낼찌, 날짜를 넣을지 추후 결정
+        
+        guard let imagePaths = diaryInfo.imagePaths else { return }
+        imagePath = imagePaths.first
     }
 }
