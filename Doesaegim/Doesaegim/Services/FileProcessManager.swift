518d6da6a2b091ededb5222c9a33e1357a66d59e
Jaehoon-So <jhspacelover@naver.com>
2022-11-23 20:04:32 +0900

    feat: 파일매니저 정의

--- Doesaegim/Doesaegim/Services/FileProcessManager.swift
+++ Doesaegim/Doesaegim/Services/FileProcessManager.swift
@@ -22,18 +22,36 @@ final class FileProcessManager {
 
 extension FileProcessManager {
     
-    func fetchImage(with imagePath: String) -> Result<UIImage, Error> {
+    func fetchImages(with imagePaths: [String]) -> [Data] {
+        var imageDatas: [Data] = []
+        imagePaths.forEach { imagePath in
+            let result = fetchImage(with: imagePath)
+            switch result {
+            case .success(let data):
+                imageDatas.append(data)
+            case .failure(let error):
+                print(error.localizedDescription)
+                // TODO: - 에러처리
+            }
+        }
+        return imageDatas
+    }
+    
+    func fetchImage(with imagePath: String) -> Result<Data, Error> {
     
         // TODO: - 더미가 아닌 파라미터로 주어진 imagePath를 사용하기
-        let dummyImagePath = "/Users/jaehoonso/Documents/default\\ image.png"
+//        let imageURL = URL(string: imagePath) -> 주석을 해제 해주세용
+        guard let dummyImageURL = URL(string: "/Users/jaehoonso/Documents/default\\ image.png") else {
+            return .failure(FileManagerError.fetchFailure(.image))
+        }
         do {
-            guard let content = try fileManager.contents(atPath: dummyImagePath) else {
-                return .fail
-            }
-            
+            let imageData = try Data(contentsOf: dummyImageURL)
+            return .success(imageData)
+        } catch {
+            return .failure(FileManagerError.fetchFailure(.image))
         }
         
-        
+        return .failure(FileManagerError.fetchFailure(.image))
     }
     
 }
