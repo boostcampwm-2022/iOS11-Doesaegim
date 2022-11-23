a8ab499bb0fd655d8d45fce2b68ca08af38bd7c1
Jaehoon-So <jhspacelover@naver.com>
2022-11-24 00:14:12 +0900

    feat: 커스텀 다이어리 콜아웃 정의

--- Doesaegim/Doesaegim/Presentation/MapScene/MapViewScene/View/DiaryCalloutView.swift
+++ Doesaegim/Doesaegim/Presentation/MapScene/MapViewScene/View/DiaryCalloutView.swift
@@ -1,20 +1,46 @@
-//
-//  DiaryCalloutView.swift
-//  Doesaegim
-//
-//  Created by Jaehoon So on 2022/11/23.
-//
-
+///
+///  DiaryCalloutView.swift
+///  Doesaegim
+///
+///  Created by Jaehoon So on 2022/11/23.
+///
+///
 import UIKit
 
-class DiaryCalloutView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+import SnapKit
+
+final class DiaryCalloutView: UIImageView {
+
+    // MARK: - Initializers
+
+    convenience init(frame: CGRect, image: UIImage?) {
+        self.init(frame: frame)
+        
+        self.image = image
+        configure()
     }
-    */
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    // MARK: - Configuration
+    
+    func configure() {
+        contentMode = .scaleAspectFit
+        configureConstraints()
+    }
+
+    func configureConstraints() {
+        snp.makeConstraints {
+            $0.width.equalTo(130)
+            $0.height.equalTo(150)
+        }
+    }
 }
