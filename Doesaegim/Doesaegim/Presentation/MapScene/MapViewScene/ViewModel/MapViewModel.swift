6de62910f6082f18e3ca139e8dafd267a9b40489
Jaehoon-So <jhspacelover@naver.com>
2022-11-23 20:05:25 +0900

    style: 필요없는 코드 삭제

--- Doesaegim/Doesaegim/Presentation/MapScene/MapViewScene/ViewModel/MapViewModel.swift
+++ Doesaegim/Doesaegim/Presentation/MapScene/MapViewScene/ViewModel/MapViewModel.swift
@@ -48,33 +48,4 @@ extension MapViewModel {
             // TODO: - 에러처리
         }
     }
-    
-//    func addDummyDiaryData() {
-//        // 3개의 데이터를 추가
-//        (0..<3).forEach { _ in
-//
-//            let dto = DiaryDTO(
-//                content: "내용입니다.",
-//                date: Date(),
-//                images: [],
-//                title: "제목"
-//            )
-//            let result = Diary.addAndSave(with: dto)
-//            switch result {
-//            case .success(let diary):
-//                // 종로
-//                let locationDTO = LocationDTO(
-//                    name: "위치",
-//                    latitude: 37.5700,
-//                    longitude: 126.979
-//                )
-//                diary.location = Location.addAndSave(with: locationDTO)
-//
-//            case .failure(let error):
-//                print(error?.localizedDescription)
-//                // TODO: - 에러처리
-//            }
-//        }
-//    }
-    
 }
