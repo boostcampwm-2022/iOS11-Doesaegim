f652e92fb6039387ce36b4893ebd4d5054e2c1aa
Jaehoon-So <jhspacelover@naver.com>
2022-11-24 14:44:46 +0900

    feat: 컬렉션 뷰 컴포지셔널 레이아웃

--- Doesaegim/Doesaegim/Presentation/DiaryScene/DiaryList/View/DiaryListViewController.swift
+++ Doesaegim/Doesaegim/Presentation/DiaryScene/DiaryList/View/DiaryListViewController.swift
@@ -5,4 +5,63 @@
 //  Created by Jaehoon So on 2022/11/24.
 //
 
-import Foundation
+import UIKit
+
+
+final class DiaryListViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // TODO: - 나중에 생성자로 주입
+    private let viewModel: DiaryListViewModelProtocol? = DiaryListViewModel()
+    
+    private lazy var collectionView: UICollectionView = {
+        
+        let layout = createCompositionalLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+    }()
+    
+    // MARK: - Initializer(s)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+    
+}
+
+extension DiaryListViewController {
+    
+    // MARK: - Collection View
+    private func createCompositionalLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { _, _ -> NSCollectionLayoutSection? in
+            let itemSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .absolute(60)
+            )
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            
+            let groupSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .absolute(60)
+            )
+            let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+            
+            let section = NSCollectionLayoutSection(group: group)
+            
+            let sectionHeaderSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .absolute(40)
+            )
+            
+            // TODO: - 헤더 등록
+            
+            // section.boundarySupplementaryItems = []
+            
+            return section
+        }
+        return layout
+    }
+    
+}
