cc6ae4c5d904aadd7b0a8a9837391502bd00de24
Jaehoon-So <jhspacelover@naver.com>
2022-11-24 14:46:29 +0900

    feat: 다이어리 뷰모델 프로토콜

---
+++ Doesaegim/Doesaegim/Presentation/DiaryScene/DiaryList/DiaryListViewModelProtocol.swift
@@ -0,0 +1,22 @@
+//
+//  DiaryListViewModelProtocol.swift
+//  Doesaegim
+//
+//  Created by Jaehoon So on 2022/11/24.
+//
+
+import Foundation
+
+protocol DiaryListViewModelProtocol: AnyObject {
+    
+    var delegate: DiaryListViewModelDelegate? { get set }
+    
+    func fetchDiary()
+    
+}
+
+protocol DiaryListViewModelDelegate: AnyObject {
+    
+    func diaryInfoDidChage()
+    
+}
