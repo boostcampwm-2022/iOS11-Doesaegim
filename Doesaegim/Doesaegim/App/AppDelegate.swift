dd5260a38f1d3d8b910c61fd0d8a611042a41292
mandos1995 <mandos19950620@gmail.com>
2022-11-29 11:37:40 +0900

    chore: AppDelegate에 firebase 초기 설정

--- Doesaegim/Doesaegim/App/AppDelegate.swift
+++ Doesaegim/Doesaegim/App/AppDelegate.swift
@@ -7,13 +7,16 @@
 
 import UIKit
 import CoreData
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
+        FirebaseApp.configure()
         return true
     }
 
