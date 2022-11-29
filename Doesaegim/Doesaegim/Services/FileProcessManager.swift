fe5fcf644f6c533e66f3830e55ac6e4f1850793e
sun <skkimeo@gmail.com>
2022-11-29 12:15:36 +0900

    feat: FileProcessManager에 저장, 삭제 메서드 추가

--- Doesaegim/Doesaegim/Services/FileProcessManager.swift
+++ Doesaegim/Doesaegim/Services/FileProcessManager.swift
@@ -5,7 +5,7 @@
 //  Created by Jaehoon So on 2022/11/23.
 //
 
-import Foundation
+import UIKit
 
 
 final class FileProcessManager {
@@ -13,14 +13,39 @@ final class FileProcessManager {
     // MARK: - Properties
     
     static let shared: FileProcessManager = FileProcessManager()
-    let fileManager: FileManager = FileManager()
+    private let fileManager: FileManager = FileManager()
+
+
     // MARK: - Initilizer(s)
     
     private init() {  }
     
 }
 
+// MARK: - Fetching and Saving Functions
 extension FileProcessManager {
+
+    // MARK: - Save Function
+
+    /// 이미지를 저장하고 성공하면 저장 위치(상대 경로)를, 실패하면 nil 리턴
+    func saveImage(_ image: UIImage, path: String) -> String? {
+        guard let data = image.jpegData(compressionQuality: Metric.compressionQuality),
+              let url = url(appendingPathComponent: path)
+        else {
+            return nil
+        }
+
+        do {
+            try data.write(to: url)
+        } catch {
+            return nil
+        }
+
+        return path
+    }
+
+
+    // MARK: - Fetch Functions
     
     func fetchImages(with imagePaths: [String]) -> [Data] {
         var imageDatas: [Data] = []
@@ -38,22 +63,55 @@ extension FileProcessManager {
     }
     
     func fetchImage(with imagePath: String) -> Result<Data, Error> {
-    
-        // TODO: - 더미가 아닌 파라미터로 주어진 imagePath를 사용하기
-//        guard let imageURL = URL(string: imagePath) else {
-//            return .failure(FileManagerError.fetchFailure(.image))
-//        }
-        guard let dummyImageURL = URL(string: "/Users/jaehoonso/Documents/default\\ image.png") else {
+        guard let url = url(appendingPathComponent: imagePath)
+        else {
             return .failure(FileManagerError.fetchFailure(.image))
         }
+
         do {
-            let imageData = try Data(contentsOf: dummyImageURL)
+            let imageData = try Data(contentsOf: url)
             return .success(imageData)
         } catch {
             return .failure(FileManagerError.fetchFailure(.image))
         }
-        
-        return .failure(FileManagerError.fetchFailure(.image))
     }
-    
+
+
+    // MARK: - Delete Functions
+
+    /// 인자로 주어진 값들을 경로로 활용해 해당 경로에 이미지가 존재하면 이미지를 리턴
+    @discardableResult
+    func deleteImage(at imagePath: String) -> Bool {
+        guard let url = url(appendingPathComponent: imagePath)
+        else {
+            return false
+        }
+
+        do {
+            try fileManager.removeItem(at: url)
+        } catch {
+            return false
+        }
+
+        return true
+    }
+
+    /// userDomainMask의 document directory에 인자를 최종 경로로 하는 url 생성
+    private func url(appendingPathComponent path: String) -> URL? {
+        guard let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first
+        else {
+            return nil
+        }
+
+        return documentsDirectory.appendingPathComponent(path.replacingOccurrences(of: "/", with: "-"))
+    }
+}
+
+
+// MARK: - Constants
+fileprivate extension FileProcessManager {
+    enum Metric {
+        /// jpeg로 압축 시 압축 품질
+        static let compressionQuality: CGFloat = 0.85
+    }
 }
