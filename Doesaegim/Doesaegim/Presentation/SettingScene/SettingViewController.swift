26a309f730b70c1da65bfb7916bcf9ffe4bd09fd
mandos1995 <mandos19950620@gmail.com>
2022-11-29 17:24:54 +0900

    feat: UIActivityViewController 공유 기능 구현

--- Doesaegim/Doesaegim/Presentation/SettingScene/SettingViewController.swift
+++ Doesaegim/Doesaegim/Presentation/SettingScene/SettingViewController.swift
@@ -9,51 +9,9 @@ import UIKit
 
 final class SettingViewController: UIViewController {
     
-    lazy var shareManager: ShareManager = ShareManager.shared
-    
-    private let testView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .red
-        return view
-    }()
-    
-    private let shareButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("버튼", for: .normal)
-        button.backgroundColor = .black
-        button.setTitleColor(UIColor.white, for: .normal)
-        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-        return button
-    }()
-    
-    @objc func buttonTapped() {
-        shareManager.instaStoryShare(sharedView: testView)
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPurple
-        view.addSubviews(testView, shareButton)
-        testView.snp.makeConstraints {
-            $0.center.equalToSuperview()
-            $0.width.height.equalTo(300)
-        }
-        shareButton.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.top.equalTo(testView.snp.bottom).offset(30)
-            $0.width.height.equalTo(100)
-        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Doesaegim/Doesaegim/Services/ShareManager.swift
+++ Doesaegim/Doesaegim/Services/ShareManager.swift
@@ -10,35 +10,56 @@ import UIKit
 final class ShareManager {
     static let shared: ShareManager = ShareManager()
     
+    var documentController: UIDocumentInteractionController?
+    
     private init() { }
     
-    func instaStoryShare(sharedView: UIView) {
+    func shareToActivityViewController(with shareView: UIView, to viewController: UIViewController) {
+        let renderer = UIGraphicsImageRenderer(size: shareView.bounds.size)
+        let renderImage = renderer.image { _ in
+            shareView.drawHierarchy(in: shareView.bounds, afterScreenUpdates: true)
+        }
+        
+        let activityViewController = UIActivityViewController(
+            activityItems: [renderImage],
+            applicationActivities: nil
+        )
+        activityViewController.excludedActivityTypes = [.saveToCameraRoll]
+        activityViewController.popoverPresentationController?.sourceView = viewController.view
+        viewController.present(activityViewController, animated: true)
+    }
+    
+    func shareToInstagramStory(with sharedView: UIView, to viewController: UIViewController) {
         guard let storyShareURL = URL(string: "instagram-stories://share") else {
-            print("URL Error")
+            viewController.presentErrorAlert(title: "공유하기 기능을 사용할 수 없어요.")
             return
         }
-        if UIApplication.shared.canOpenURL(storyShareURL) {
-            let renderer = UIGraphicsImageRenderer(size: sharedView.bounds.size)
-            
-            let renderImage = renderer.image { _ in
-                sharedView.drawHierarchy(in: sharedView.bounds, afterScreenUpdates: true)
-            }
-            guard let imageData = renderImage.pngData() else { return }
-            
-            let pasteboardItems: [String: Any] = [
-                "com.instagram.sharedSticker.stickerImage": imageData,
-                "com.instagram.sharedSticker.backgroundTopColor": "#636e72",
-                "com.instagram.sharedSticker.backgroundBottomColor": "#b2bec3",
-            ]
-            
-            let pasteboardOptions = [
-                UIPasteboard.OptionsKey.expirationDate: Date().addingTimeInterval(300)
-            ]
-            
-            UIPasteboard.general.setItems([pasteboardItems], options: pasteboardOptions)
-            UIApplication.shared.open(storyShareURL, options: [:], completionHandler: nil)
-        } else {
-            print("can't open")
+        guard UIApplication.shared.canOpenURL(storyShareURL) else {
+            viewController.presentErrorAlert(title: "인스타그램 앱을 설치하셔야 이용할 수 있어요.")
+            return
         }
+        let renderer = UIGraphicsImageRenderer(size: sharedView.bounds.size)
+        
+        let renderImage = renderer.image { _ in
+            sharedView.drawHierarchy(in: sharedView.bounds, afterScreenUpdates: true)
+        }
+        guard let imageData = renderImage.pngData() else {
+            viewController.presentErrorAlert(title: "공유할 수 없는 이미지 유형입니다.")
+            return
+        }
+        
+        let pasteboardItems: [String: Any] = [
+            "com.instagram.sharedSticker.stickerImage": imageData,
+            "com.instagram.sharedSticker.backgroundTopColor": "#636e72",
+            "com.instagram.sharedSticker.backgroundBottomColor": "#b2bec3"
+        ]
+        
+        let pasteboardOptions = [
+            UIPasteboard.OptionsKey.expirationDate: Date().addingTimeInterval(300)
+        ]
+        
+        UIPasteboard.general.setItems([pasteboardItems], options: pasteboardOptions)
+        UIApplication.shared.open(storyShareURL, options: [:], completionHandler: nil)
     }
+    
 }
