fd6a8c365d1321bbbc2554a1bfabcfbcc56f34c7
Jaehoon-So <jhspacelover@naver.com>
2022-12-07 17:19:10 +0900

    feat: 테이블 뷰 정의

--- Doesaegim/Doesaegim/Presentation/SettingScene/SettingViewController.swift
+++ Doesaegim/Doesaegim/Presentation/SettingScene/SettingViewController.swift
@@ -9,9 +9,66 @@ import UIKit
 
 final class SettingViewController: UIViewController {
     
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return table
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPurple
     }
 
+}
+
+extension SettingViewController {
+    
+    private func configure() {
+        
+        configureNavigationBar()
+        configureTableView()
+        configureSubviews()
+        configureConstraints()
+        
+    }
+    
+    private func configureNavigationBar() {
+        navigationItem.title = "설정"
+    }
+    
+    private func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
+    }
+    
+    private func configureSubviews() {
+        
+    }
+    
+    private func configureConstraints() {
+        
+    }
+    
+    
+    
+}
+
+extension SettingViewController: UITableViewDelegate {
+    
+}
+
+extension SettingViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello World"
+        return cell
+    }
 }
