c74c24af7e67c5db064711f27acf15c164a7f6a6
Jaehoon-So <jhspacelover@naver.com>
2022-12-09 17:48:26 +0900

    fix: conflict 해결 중, SettingOptionViewModel 구조체 분실 해결

---
+++ Doesaegim/Doesaegim/Data/ViewModel/SettingOptionViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  SettingOptionViewModel.swift
+//  Doesaegim
+//
+//  Created by Jaehoon So on 2022/12/09.
+//
+
+import UIKit
+
+
+struct SettingOptionViewModel {
+    
+    let title: String
+    let icon: UIImage?
+    let iconTintColor: UIColor?
+    let handler: (() -> Void)
+    
+}
+
+enum SettingOptionType {
+    case staticCell(model: SettingOptionViewModel)
+    case switchCell(model: SettingOptionViewModel)
+}
